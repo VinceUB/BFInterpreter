b535f683ccdaac40e2ffc07d355680a5d53fffed
vkb24312 <vincent.bechmann@gmail.com>
2018-10-22 20:25:49 +0300

    Redirected all exceptions to the err stream

--- src/main/java/io/github/vkb24312/BFInterpreter/Interpreter.java
+++ src/main/java/io/github/vkb24312/BFInterpreter/Interpreter.java
@@ -6,120 +6,118 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.nio.charset.Charset;
 
-public class Interpreter {
-    String code;
-    int index = 0;
-    char codeAt(int index){
+class Interpreter {
+    private String code;
+    private int index = 0;
+    private char codeAt(int index){
         return code.charAt(index);
     }
 
-    PrintStream out;
-    PrintStream err;
-    InputStream in;
+    private PrintStream out;
+    private PrintStream err;
+    private InputStream in;
 
-    byte[] cells = new byte[30000];
-    int pointer = 0;
+    private byte[] cells = new byte[30000];
+    private int pointer = 0;
 
-
-    void add(){
+    //region Actions? Methods? Commands? Dunno what to call them. They're just methods that are called for each symbol
+    private void add(){
         if((cells[pointer]&0xff) >= 255){
             cells[pointer] = 0;
         } else {
             cells[pointer]++;
         }
     }
-
-    void subtract(){
+    private void subtract(){
         if((cells[pointer&0xff]) <= 0){
             cells[pointer] = -1;
         } else {
             cells[pointer]--;
         }
     }
-
-    void pointerUp(){
+    private void pointerUp(){
         if(pointer>=cells.length){
             pointer = 0;
         } else {
             pointer++;
         }
     }
-
-    void pointerDown(){
+    private void pointerDown(){
         if(pointer<=0){
             pointer = cells.length;
         } else {
             pointer--;
         }
     }
-
-    void prompt(){
+    private void prompt(){
         try{
             cells[pointer] = ((byte) in.read());
         } catch (IOException e){
             e.printStackTrace(err);
         }
     }
-
-    void print(){
+    private void print(){
         out.print((char) (cells[pointer]&0xff));
 
         if(out.checkError()){
             err.println("There was an error in output");
         }
     }
-
-    void openLoop(){
+    private void openLoop(){
         if(cells[pointer]==0){
             index = findPair(index);
         }
     }
-
-    void closeLoop(){
+    private void closeLoop(){
         if(cells[pointer]!=0)
         index = findPair(index);
     }
+    //endregion
 
 
-    public void run(){
-        for (index = 0; index < code.toCharArray().length; index++) {
-            switch(codeAt(index)){
-                case '+':
-                    add();
-                    break;
-                case '-':
-                    subtract();
-                    break;
-                case '>':
-                    pointerUp();
-                    break;
-                case '<':
-                    pointerDown();
-                    break;
-                case '.':
-                    print();
-                    break;
-                case ',':
-                    prompt();
-                    break;
-                case '[':
-                    openLoop();
-                    break;
-                case ']':
-                    closeLoop();
-                    break;
+    void run(){
+        try {
+            for (index = 0; index < code.toCharArray().length; index++) {
+                switch (codeAt(index)) {
+                    case '+':
+                        add();
+                        break;
+                    case '-':
+                        subtract();
+                        break;
+                    case '>':
+                        pointerUp();
+                        break;
+                    case '<':
+                        pointerDown();
+                        break;
+                    case '.':
+                        print();
+                        break;
+                    case ',':
+                        prompt();
+                        break;
+                    case '[':
+                        openLoop();
+                        break;
+                    case ']':
+                        closeLoop();
+                        break;
+                }
             }
+        } catch (Exception e){
+            e.printStackTrace(err);
         }
     }
 
-    public Interpreter(String code, PrintStream output, InputStream input, PrintStream error){
+    Interpreter(String code, PrintStream output, InputStream input, PrintStream error){
         this.code = code;
         out = output;
         in = input;
         err = error;
     }
 
-    int findPair(int index){
+    private int findPair(int index){
         if(codeAt(index)=='['){
 
             int indent = 0;
