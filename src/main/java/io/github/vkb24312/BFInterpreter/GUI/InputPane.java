07a6dace7a1581e058019a2ef209b046e51ba4fa
Vincent <vincent.bechmann@gmail.com>
2018-10-23 14:47:33 +0300

    Added fonts

--- src/main/java/io/github/vkb24312/BFInterpreter/GUI/InputPane.java
+++ src/main/java/io/github/vkb24312/BFInterpreter/GUI/InputPane.java
@@ -1,26 +1,48 @@
 package io.github.vkb24312.BFInterpreter.GUI;
 
 import javax.swing.*;
-import javax.swing.event.CaretEvent;
-import javax.swing.event.CaretListener;
+import javax.swing.event.HyperlinkEvent;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
+import javax.swing.text.html.HTMLDocument;
 import java.awt.*;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class InputPane extends JTextPane {
     private final static String defaultText = "Enter your code here";
 
-    StyleContext context = new StyleContext();
-    Style ghostText = context.addStyle("ghost", null);
+    private StyleContext context = new StyleContext();
+    private Style ghostText = context.addStyle("ghost", null);
+    private Style code = context.getStyle("default");
 
     public InputPane() {
         StyleConstants.setForeground(ghostText, Color.GRAY);
+        try {
+            setFont(Font.createFont(Font.TRUETYPE_FONT, this.getClass().getClassLoader().getResourceAsStream("DejaVuSansMono.ttf")).deriveFont(12.0f));
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            setText("\nThere seems to have been a problem loading the font. Perhaps you messed about in the jar file?\n" +
+                    " If you haven't deleted or modified anything in the jarfile, please contact me on github (https://github.com/vkb24312/BFInterpreter/issues) \n");
+            e.printStackTrace(new PrintStream(new OutputStream() {
+                @Override
+                public void write(int b){
+                    setText(getText() + (char) b);
+                }
+            }));
+            e.printStackTrace();
+        }
 
         try {
             getStyledDocument().insertString(0, defaultText, ghostText);
@@ -33,7 +55,7 @@ public class InputPane extends JTextPane {
             public void keyTyped(KeyEvent e) {
                 if(getText().equals(defaultText)){
                     setText("");
-                    setCharacterAttributes(context.getStyle("default"), true);
+                    setCharacterAttributes(code, true);
                 } else if(getText().isEmpty()){
                     try {
                         getStyledDocument().insertString(0, defaultText, ghostText);
